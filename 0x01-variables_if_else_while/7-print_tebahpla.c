9469aafc7411b2356d9595084d975df1d2c99dea
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-18 16:05:10 -0500

    Added Task 7 C File

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 122;
+
+	while (alphabet >= 97)
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar(10);
+	return (0);
+}
