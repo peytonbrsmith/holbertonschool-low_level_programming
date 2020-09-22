fca434b0475d00c474110cae82808fc2e86473f2
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-22 15:54:22 -0500

    updated file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a > b && a > c) || (a > b && a == c) || (a == b && a > c))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b > a && b > c) || (b > a && b == c) || (b == c && b > a))
 	{
 		largest = b;
 	}
-	else if (c > a && c > b) 
+	else if ((c > a && c > b) || (c > b && c == a) || (c == b && c > a))
 	{
 		largest = c;
 	}
