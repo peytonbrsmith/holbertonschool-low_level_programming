981f6cdd870ea4c57550d178a5e2ff77577ded73
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-23 14:21:15 -0500

    added times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,11 @@ void times_table(void)
 {
 	int x, y, z;
 
-	for (x= 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-
 			if (z < 10 && y != 0)
 			{
 				_putchar(' ');
@@ -32,10 +31,8 @@ void times_table(void)
 			{
 				_putchar(z + '0');
 			}
-
 			if (y < 9)
 				_putchar(',');
-
 		}
 		_putchar('\n');
 	}
