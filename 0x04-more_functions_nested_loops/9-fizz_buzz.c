591bce09e4dcbd6992b884cd26eeb86fed47be5c
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-24 14:09:12 -0500

    Added fizz buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && !(i % 5 == 0))
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5 == 0) && !(i % 3 == 0))
 		{
 			printf("Buzz ");
 		}
@@ -24,10 +24,10 @@ int main(void)
 			printf("FizzBuzz ");
 		}
 		else
-			printf("%d ",i);
+			printf("%d ", i);
 	}
 
 	printf("\n");
 
-    return (0);
+	return (0);
 }
