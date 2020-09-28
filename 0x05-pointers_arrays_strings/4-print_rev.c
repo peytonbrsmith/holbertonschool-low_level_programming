a3dbb62bf00e04b3a538fa2426702a4c71f4c8b6
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-28 16:56:48 -0500

    Added multiple tasks and updated header file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - puts a string to stdout reversed
+ * @str: the string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
+	_putchar(10);
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints an array of n integers
+ * @a: The array
+ * @n: the number of elements to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	for (int i=0; n >= 1; n--, i++)
+	{
+		printf("%d",a[i]);
+		if (n > 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
