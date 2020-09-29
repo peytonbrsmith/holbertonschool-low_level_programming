88c98e45f07465ebf5c63e76dc7fff92dddae337
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-29 14:03:22 -0500

    Added rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rev_string - puts a string to stdout reversed
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int up, down;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	char temps[len];
+
+	for (up = 0, down = (len - 1); up < len; up++, down--)
+	{
+		*(temps + down) = *(s + up);
+	}
+
+	for (up = 0; up < len; up++)
+	{
+		*(s + up) = *(temps + up);
+	}
+}
