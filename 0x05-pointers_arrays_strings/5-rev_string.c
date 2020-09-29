fa3b73db8734e29352f6a3aa585a645a1fc6c2cc
Peyton Smith <peytonbrsmith@gmail.com>
2020-09-29 14:36:32 -0500

    Added rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,20 +10,16 @@
 void rev_string(char *s)
 {
 	int len = 0;
-	int temps[10];
 	int up, down;
 
 	while (*(s + len) != '\0')
 		len++;
 	len--;
 
-	for (up = 0, down = len; up <= len; up++, down--)
+	for (up = 0, down = len; up < down; up++, down--)
 	{
-		*(temps + down) = *(s + up);
-	}
-
-	for (up = 0; up <= len; up++)
-	{
-		*(s + up) = *(temps + up);
+		char temp = *(s + up);
+		*(s + up) = *(s + down);
+		*(s + down) = temp;
 	}
 }
