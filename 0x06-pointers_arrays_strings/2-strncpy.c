91d02dc495772c14362a318d7f83abf14f2d52ba
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-01 18:51:48 +0000

    Updated strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (*(src + len1) != '\0')
 		len1++;
 
-	for (i = 0; i < len1 && i <= n; i++)
+	for (i = 0; i < len1 && i < n; i++)
 	{
 		*(dest + (i)) = *(src + i);
 	}
