0eff28afd6e118fc6a950509733c74d095de49a5
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-02 17:22:47 +0000

    Added task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,24 +5,24 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: 
+ * Return: an int value less than greater to or equal to 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-    int ret;
+	int ret;
 
-    while(*s1 && *s2)
-    {
-        if (*s1 == *s2)
-        {
-            s1++;
-            s2++;
-        }
-        else
-            break;
-    }
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			break;
+	}
 
-    ret = *s1 - *s2;
-    return (ret);
+	ret = *s1 - *s2;
+return (ret);
 }
