352d282de1a4150e9986e84ec724bae7a9939e40
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-05 13:59:12 -0500

    Added task #3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the prefix substring
+ * @accept: the string of bytes to count
+ *
+ * Return:  the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int o = 0;
+	int i;
+
+	while (*accept)
+	{
+		for (i = 0; s[i] != ','; i++)
+		{
+			if (s[i] == *accept)
+			{
+				o++;
+			}
+		}
+		accept++;
+	}
+	return (o);
+}
