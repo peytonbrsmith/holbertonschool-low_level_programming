20ecf914f4ac80eae179ad1d7f41a63c3a427245
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-05 16:00:34 -0500

    Updated Task #2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,9 +12,9 @@ char *_strchr(char *s, char c)
 	char *ptr;
 	int i;
 
-	for (i = 0; *s >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*(s + i) == c)
+		if (s[i] == c)
 		{
 			ptr = s + i;
 			return (ptr);
