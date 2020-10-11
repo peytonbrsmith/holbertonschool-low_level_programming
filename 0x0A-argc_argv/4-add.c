46b8e379c893d821c61620a1d5657ffbc993529a
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-11 16:34:48 -0500

    Made negative numbers not cause error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(*argv[i]) == 0)
+		if (isdigit(*argv[i]) == 0 && *argv[i] != '-')
 		{
 			printf("Error\n");
 			return (1);
