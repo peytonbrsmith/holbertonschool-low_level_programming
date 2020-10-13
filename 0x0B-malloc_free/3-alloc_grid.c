3c5dace2519ed749603c266d48bb1ea232de44cc
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-13 15:12:28 -0500

    Added Task #3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * alloc_grid - allocates memory for grid of width and height
+ * @width: width
+ * @height: height
+ * Return: pointer to array or null
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ar;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ar = malloc(height * sizeof(int *));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ar[i] = malloc(width * sizeof(int));
+		if (ar == NULL)
+			return (NULL);
+	}
+	return (ar);
+}
