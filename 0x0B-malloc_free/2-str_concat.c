8c10667a7d0393da63ee4e07763023007648089a
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-13 14:03:46 -0500

    Updated task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,11 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int i, j;
 
-	s3 = malloc(sizeof(s1) + sizeof(s2));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s3 = malloc((sizeof(s1) + sizeof(s2)) - 1);
 	if (s3 == NULL)
 		return (NULL);
 
