ca50337d1aa591c45a963c18a291620f8ddf0d5f
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-13 12:35:41 -0500

    Updated Task #1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(__attribute__ ((unused)) char *str)
 
 	if (str[0] == '\0')
 		return (NULL);
-	for (size = 0; *(str + size) != '\0'; size++)
+	for (size = 1; *(str + size) != '\0'; size++)
 		;
 	ar = malloc((size + 1) * sizeof(char));
 	if (ar == NULL)
