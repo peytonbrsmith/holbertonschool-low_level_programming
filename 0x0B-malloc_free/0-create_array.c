45609d6c3e7451dd60b40981545bd8e574113772
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-13 12:15:04 -0500

    Updated first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
 	ar = malloc(size * sizeof(char));
-
+	if (ar == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		ar[i] = c;
