69ba8c0a73775c1065bd4569ecda27f47ce02b84
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-14 18:36:18 +0000

    updated task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		ar[i] = malloc(width * sizeof(int));
 		if (ar[i] == NULL)
 		{
-			for (; i > 0; i--)
+			for (; i >= 0; i--)
 				free(*(ar + i));
 			free(ar);
 			return (NULL);
