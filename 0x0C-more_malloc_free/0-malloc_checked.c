7e4a0a0d1eecadf13d4568942df8619834cd2bbc
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-15 10:04:52 -0500

    Updated task #1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
 
+/**
+ * malloc_checked - allocates memory and checks for failure
+ * @b: the size in bytes to allocate 
+ *
+ * Return: exits with 98 on fail. ptr to memory on success
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ar;
 
-	ar = (void *)malloc(b);
+	ar = malloc(b);
 	if (ar == NULL)
 		exit (98);
 	return(ar);
