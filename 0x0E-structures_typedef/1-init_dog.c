4ee4288e6bb3c938fbd710ed61481a666a349674
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-19 14:13:59 -0500

    Added task 1 and updated dog.h with all prototypes

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog struct with info
+ * @d: pointer to the struct
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to age of owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
