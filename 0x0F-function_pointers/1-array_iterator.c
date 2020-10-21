9cd4eb478037c0b260bb3547257f6ea1ff33a69e
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-21 14:04:06 -0500

    Added Task #1 and updated header

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iteratates an array through a function
+ * @array: the array of integers
+ * @size: the size of the array
+ * @action: the function pointer
+ *
+ * Return void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+}
