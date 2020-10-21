e264f688d4f8322443071ef3d75a14660cad43d8
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-21 14:54:11 -0500

    updated task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,15 +1,16 @@
 #include "function_pointers.h"
 /**
- * array_iterator - iteratates an array through a function
+ * int_index - runs multiples comparison passes over an array
  * @array: the array of integers
  * @size: the size of the array
- * @action: the function pointer
+ * @cmp: the function pointer
  *
- * Return void
+ * Return: the index of the array or -1
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i, res;
 
 	if (cmp == NULL)
 		return (-1);
@@ -19,7 +20,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (((*cmp)(array[i])) == 1)
+		res = (*cmp)(array[i]);
+		if (i == (size - 1) && res != 1)
+			return (-1);
+		if (res == 1)
 			break;
 	}
 	return (i);
