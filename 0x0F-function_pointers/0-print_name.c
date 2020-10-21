1294a5f8eb08a2b40c3759b72471dadda9d96059
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-21 13:21:58 -0500

    Added README and task 0 in new directory

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - calls the function pointer with a char pointer
+ * @name: string passed
+ * @f: function pointer passed
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H_ */
