9758820b99b08eba4cae0b612b9a8e8d7d12b727
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-22 10:31:36 -0500

    Updated task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,6 +7,24 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	s = s;
-	return (0);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (*s == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,13 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int num1, num2, result;
 
 	if (argc < 4)
 		return (-1);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	(*get_op_func(argv[2]))(num1, num2);
+	result = (*get_op_func(argv[2]))(num1, num2);
+	printf("%d\n", result);
 	return (0);
 }
