b528d87bc033fa9a6c5c590f1eb0ed4c65cbcab6
Peyton Smith <peytonbrsmith@gmail.com>
2020-10-22 11:02:03 -0500

    Updated task 3 files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -26,5 +26,5 @@ int (*get_op_func(char *s))(int, int)
 		i++;
 	}
 	printf("Error\n");
-	exit (99);
+	exit(99);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,10 @@ int main(int argc, char *argv[])
 	int num1, num2, result;
 
 	if (argc != 4)
+	{
+		printf("Error\n");
 		exit (98);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
