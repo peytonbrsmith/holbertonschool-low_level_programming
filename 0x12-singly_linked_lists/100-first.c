6bf20d6b24b1ea7fb703fae67c21c80cc6bfe594
peytonbrsmith <peytonbrsmith@gmail.com>
2020-10-30 12:49:32 -0500

    Updated advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 void beforeMain(void) __attribute__ ((constructor));
 /**
  * beforeMain - executes code before main
  */
 void beforeMain(void)
 {
-printf(
-"You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
 }
