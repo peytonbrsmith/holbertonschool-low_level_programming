e8187aca1d8adc754f873ddfc1a23b17a83f3e14
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-04 14:33:07 -0600

    Added advanced tasks

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a linked list
+ * @h: double pointer to head
+ * Return: the size that was free'd
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	h = h;
+	return (0);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * find_listint_loop - finds a loop in a linked list
+ * @head: double pointer to head
+ * Return: the address of the node that starts it
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	head = head;
+	return (head);
+}
