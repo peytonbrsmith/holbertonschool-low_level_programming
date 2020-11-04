9d1d20a110b537e729c1dacfdb0b70522e4b6de1
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-04 12:37:43 -0600

    Removed redundant listint_t va

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,15 +10,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *current = head;
-	listint_t *next;
 
+	if (head == NULL)
+		return (NULL);
 	while (i < index)
 	{
 		if (head == NULL)
 			return (NULL);
-		next = current->next;
-		current = next;
+		current = current->next;
 		i++;
 	}
-	return (next);
+	return (current);
 }
