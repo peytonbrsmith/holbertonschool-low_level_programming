398ecbd91388968bbe7a9a42bc86e9fcba6f7895
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-04 14:15:10 -0600

    added empty advanced task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a linked list of integers
+ * @head: pointer to head
+ * Return: pointer to head
+ */
+
+listint_t *reverse_listint(listint_t **head);
+{
+	head = head;
+
+	return (head);
+}
