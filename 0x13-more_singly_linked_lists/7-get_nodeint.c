8808cfb0fb80ad3ad612e66dfa47fae297b5da23
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-04 14:19:13 -0600

    Edited NULL check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	while (i < index)
 	{
-		if (head == NULL)
+		if (current == NULL)
 			return (NULL);
 		current = current->next;
 		i++;
