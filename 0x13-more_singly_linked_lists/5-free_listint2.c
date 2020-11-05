bc17f9272443c36afe4ff8b33df4ede41974efb0
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-05 10:48:19 -0600

    Added null check within loop

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,17 +5,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *current = *head;
 	listint_t *next;
 
 	if (*head == NULL)
 		return;
-	current = *head;
 	while (current != NULL)
 	{
 		next = current->next;
-
-		free(current);
+		if (current != NULL)
+			free(current);
 		current = next;
 	}
 	*head = NULL;
