2b22226052254768b77ea771ae1a64abcad0585c
Peyton Smith <peytonbrsmith@gmail.com>
2020-11-06 20:11:18 +0000

    Moved definition after NULL check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,11 +5,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	listint_t *next;
 
 	if (head == NULL)
 		return;
+	current = *head;
 	while (current != NULL)
 	{
 		next = current->next;
