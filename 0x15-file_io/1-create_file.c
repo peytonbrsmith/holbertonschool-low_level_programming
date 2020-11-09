4d2d0f1caf8b792961666e0249997ef85dde69fa
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-09 14:43:16 -0600

    Removed uplicate while loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,27 +9,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, i = 0;
 
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
-                	i++;
+			i++;
 
 		wr = write(fd, text_content, i);
 		if (wr == -1)
 			return (-1);
 	}
-
 	return (1);
 }
