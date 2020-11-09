6d04f898bffb8b82b3ad499c7827ae3cb432a8bd
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-09 14:31:47 -0600

    Added NULL check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,20 @@ int create_file(const char *filename, char *text_content)
 	{
 		i++;
 	}
+
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	wr = write(fd, text_content, i);
-	if (wr == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		wr = write(fd, text_content, i);
+		if (wr == -1)
+			return (-1);
+	}
 
 	return (1);
 }
