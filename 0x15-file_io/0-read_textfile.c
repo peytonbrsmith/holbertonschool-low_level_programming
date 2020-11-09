df3786a0f02b9dd7c32d8addbecb23a9ed8355ca
peytonbrsmith <peytonbrsmith@gmail.com>
2020-11-09 13:40:05 -0600

    fixed betty requirement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	wr = write(STDOUT_FILENO, buf, rd);
 	if (wr == -1)
-                return (0);
+		return (0);
 
 	close(fd);
 	free(buf);
