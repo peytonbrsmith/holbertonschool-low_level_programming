a0e1d766941b9b3ba104907a2447ab71cd58f29f
Peyton Smith <peytonbrsmith@gmail.com>
2020-11-10 20:00:08 +0000

    created while loop to continue if there are more bytes to be read

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int ac, char **av)
 {
 	int fd1, fd2, rd, wr;
-	char *buf;
+	char buf[1024];
 
 	if (ac != 3)
 	{
@@ -17,26 +17,21 @@ int main(int ac, char **av)
 	}
 
 	fd1 = open(av[1], O_RDONLY);
-
-	buf = malloc(1024);
-	if (buf == NULL)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+	fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 
 	rd = read(fd1, buf, 1024);
+	wr = write(fd2, buf, rd);
+	while (rd > 0)
+	{
+		rd = read(fd1, buf, 1024);
+		wr = write(fd2, buf, rd);
+	}
 	if (fd1 == -1 || rd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-
-	wr = write(fd2, buf, rd);
-
 	close(fd1);
 	close(fd2);
-	free(buf);
 	return (wr);
 }
