f8b031b1e0c946b36f9f23540ab523180f78935f
Peyton Smith <peytonbrsmith@gmail.com>
2020-11-10 21:07:38 +0000

    Updated task 3 with more error messages

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,37 +1,56 @@
 #include "holberton.h"
 /**
+ * clos - close errors
+ * @fd: the file descriptor to print
+ */
+void clos(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %i", fd);
+	exit(100);
+}
+/**
  * main - copies content of one file to another
  * @ac: arg count
- * @av: argument pointers
+ * @a: argument pointers
  * Return: number of successfully copied chars
  */
-int main(int ac, char **av)
+int main(int ac, char **a)
 {
-	int fd1, fd2, rd, wr;
+	int fd1, fd2, rd = 1, wr, cl, cl2;
 	char buf[1024];
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(2, "Usage: %s file_from file_to\n", a[0]);
 		exit(97);
 	}
-
-	fd1 = open(av[1], O_RDONLY);
-	fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-
-	rd = read(fd1, buf, 1024);
-	wr = write(fd2, buf, rd);
-	while (rd > 0)
+	fd1 = open(a[1], O_RDONLY);
+	if (fd1 == -1)
 	{
-		rd = read(fd1, buf, 1024);
-		wr = write(fd2, buf, rd);
-	}
-	if (fd1 == -1 || rd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(2, "Error: Can't read from file %s\n", a[1]);
 		exit(98);
 	}
-	close(fd1);
-	close(fd2);
+	fd2 = open(a[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+while (rd > 0)
+{
+rd = read(fd1, buf, 1024);
+if (rd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", a[1]);
+exit(98);
+}
+wr = write(fd2, buf, rd);
+if (wr == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", a[2]);
+exit(99);
+}
+	}
+	cl = close(fd1);
+	if (cl == -1)
+		clos(fd1);
+	cl2 = close(fd2);
+	if (cl2 == -1)
+		clos(fd2);
 	return (wr);
 }
