1fa81419310412f33f6fb6a37553d3e0e38f9a96
peytonbrsmith <peytonbrsmith@gmail.com>
2020-12-13 11:17:24 -0600

    removed typo in prototype

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  *
  * Return: node at index or NULL
  */
-ddlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *current = head;
