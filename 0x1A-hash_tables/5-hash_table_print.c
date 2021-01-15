65773d0c52427a4f5042af4b6d43a327bc6bd524
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-15 20:49:36 +0000

    added task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash tables
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *ptr = NULL;
+	int needcomma = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			for (ptr = ht->array[i]; ptr; ptr = ptr->next)
+			{
+				if (needcomma == 1)
+					printf(", ");
+				printf("'%s': '%s'", ptr->key, ptr->value);
+				needcomma = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
